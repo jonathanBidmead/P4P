0b2f0405cc861ece19415cabf4aec78f66c5e5e1
jonathanBidmead <80360821+jonathanBidmead@users.noreply.github.com>
2021-07-13 14:41:51 +1200

    "Fixed" Movement bug where KR10 only travelled some portion of its allotted path
    "Fixed" by just re-sending co-ordinates until the position is close enough to the setpoint (abs diff < espilon). Still not quite working, some issues with timeouts to deal with

--- KukaVarProxy/py_KVP_Test/opcua_Client_KVP_Test.py
+++ KukaVarProxy/py_KVP_Test/opcua_Client_KVP_Test.py
@@ -1,3 +1,4 @@
+from time import sleep
 from opcua import Client
 url = "localhost"
 port = 7001
@@ -16,10 +17,15 @@ print(method)
 
 getCurrentPosition = method[2]
 moveKR10 = method[3]
+moveKR16 = method[4]
+moveKR16_rel = method[5]
 x = 0
 y = 0
 z = 0
 pointA = [900,100,1114,180,0,-180]
 pointB = [-85.2,-583.7,1124.3,1,-0.5,178.4]
 pointC = [-85.2+x,-583.7+y,1124.3+z,1,-0.5,178.4]
-objects_node.call_method(moveKR10,pointC)
+pointD = [-50,-100,0,0,0,0]
+objects_node.call_method(moveKR16,pointC)#TODO: OPCUA Expects response from server within 4s. Robot Traversal takes longer than this. Solution: Have sub-function within KVP/OPCUA Server that does the actual moving, define flag for movement complete, etc
+sleep(10)
+objects_node.call_method(moveKR16_rel,pointD)
--- KukaVarProxy/py_KVP_Test/opcua_Server_KVP_test.py
+++ KukaVarProxy/py_KVP_Test/opcua_Server_KVP_test.py
@@ -5,6 +5,7 @@ from time import sleep
 from datetime import datetime
 import sys
 from py_openshowvar import openshowvar
+from math import sqrt
 
 # create instance/object of type opcua.Server
 KVP_Server = opcua.Server()
@@ -51,6 +52,13 @@ def getCurrentPos_Internal():
     print(pos[12])
     return([float(pos[2]),float(pos[4]),float(pos[6]),float(pos[8]),float(pos[10]),float(pos[12])])
 
+def sqrt_of_squares(input):
+    sumInternal = 0
+    for i in input:
+        sumInternal += i*i
+    return sqrt(sumInternal)
+
+
 @uamethod
 def getCurrentPosition(parent):
     pos_string = KVP_client.read('$POS_ACT', debug=False)
@@ -87,9 +95,63 @@ def moveDirectKR10(parent, point):
         debug_rite_var = KVP_client.write('my_inc',pos_asString,debug=False)
         debug_write_var = KVP_client.write('my_step','TRUE',debug=False)
     
+#Not working properly atm. The idea is that this keeps sending the same point to the robot until it actually reaches the point, but there 
+#must be some mathematical error somewhere because I think the point it's telling the robot to go to keeps changing (badly)
+@uamethod
+def moveDirectKR16(parent, point):
+    epsilon = 0.1
+    initialPos = getCurrentPos_Internal()
+    point_check = sqrt_of_squares(point)
+    newPos_check = point_check + epsilon + 10 #arbitrary value added so the loop can start
+    
+    while abs(newPos_check - point_check) > epsilon:#TODO: This condition kinda sucks, edge case where multiple XYZABCs result in same sqrt sum
+        print("Point Check", point_check)
+        print("new pos check", newPos_check)
+        initialPos = getCurrentPos_Internal()
 
+        
+        point_rel = [0,0,0,0,0,0]
+
+        for p in range(6):
+            point_rel[p] = point[p] - initialPos[p]
+        print("Function Called")
+        
+        if (KVP_client.read('my_step').decode('utf-8') == 'FALSE'):
+            newPos = getCurrentPos_Internal()
+            newPos_check = sqrt_of_squares(newPos)
+
+            debug_write_var = KVP_client.write('MYX',str(point_rel[0]),debug=False)
+            debug_write_var = KVP_client.write('MYY',str(point_rel[1]),debug=False)
+            debug_write_var = KVP_client.write('MYZ',str(point_rel[2]),debug=False)
+            debug_write_var = KVP_client.write('MYA',str(point_rel[3]),debug=False)
+            debug_write_var = KVP_client.write('MYB',str(point_rel[4]),debug=False)
+            debug_write_var = KVP_client.write('MYC',str(point_rel[5]),debug=False)
+            debug_write_var = KVP_client.write('MYE1',str(0),debug=False)#TODO: Implement E1 Properly
+            KVP_client.write('my_step','TRUE')
+            print("Point Sent to Robot")
+        sleep(1)
+        print("Looped")
+
+
+
+@uamethod
+def moveDirectKR16_relative(parent, point):
+    point_rel = point
+    print("Function Called")
+    if (KVP_client.read('my_step').decode('utf-8') == 'FALSE'):
+        debug_write_var = KVP_client.write('MYX',str(point_rel[0]),debug=False)
+        debug_write_var = KVP_client.write('MYY',str(point_rel[1]),debug=False)
+        debug_write_var = KVP_client.write('MYZ',str(point_rel[2]),debug=False)
+        debug_write_var = KVP_client.write('MYA',str(point_rel[3]),debug=False)
+        debug_write_var = KVP_client.write('MYB',str(point_rel[4]),debug=False)
+        debug_write_var = KVP_client.write('MYC',str(point_rel[5]),debug=False)
+        debug_write_var = KVP_client.write('MYE1',str(0),debug=False)#TODO: Implement E1 Properly
+        KVP_client.write('my_step','TRUE')
+        print("Point Sent to Robot")
 
 objects.add_method(1, "moveKR10", moveDirectKR10)
+objects.add_method(1, "moveKR16", moveDirectKR16)
+objects.add_method(1, "moveKR16_rel", moveDirectKR16_relative)
 # starting! The server will continue running
 try:
     current_time = str(datetime.now().time())[:-7]
