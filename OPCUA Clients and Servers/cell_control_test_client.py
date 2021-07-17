0af1d85a2dd6bf762e631a54419c6913f2b6ca96
jonathanBidmead <80360821+jonathanBidmead@users.noreply.github.com>
2021-07-17 13:20:51 +1200

    Created Cell Control Client and Server Skeletons

---
+++ OPCUA Clients and Servers/cell_control_test_client.py
@@ -0,0 +1,23 @@
+from opcua import Client
+
+url = "localhost"
+port = 7001
+end_point = "opc.tcp://{}:{}".format(url, port)
+cell_client = Client(end_point)
+cell_client.connect()
+
+# Get the root node of the adress space
+objects_node = cell_client.get_objects_node()
+
+# Get the children node of the objects Method
+method = objects_node.get_children()
+
+print(method)
+
+for node in method:
+    print(node.get_browse_name().to_string())
+
+print(method[1].get_children()[0].get_value())
+print(method[1].get_children()[0].set_value(3))
+print(method[1].get_children()[0].get_value())
+
